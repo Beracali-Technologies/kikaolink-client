d5f8201e2b498023cc7ffb0dc63be53be2131076
kelvin magwe <kelvinmullar5@gmail.com>
2025-01-08 17:20:37 -0600

    Successfully deployed frontend to the vercel

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-                "./src/**/*.{js,ts,tsx,jsx}",
-                "./src/components/**/*.{js,ts,jsx,tsx}",
-                "./src/*.{js,jsx,ts,tsx}",
-           ],
+            "./src/**/*.{js,ts,tsx,jsx}",
+            "./src/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
