a8f352fca3cba1f2d5a8b3e41ac9a5a26d80948f
kelvin magwe <kelvinmullar5@gmail.com>
2025-01-08 15:42:33 -0600

    Made change to import change unused in App.tsx to make deploy on vercel

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-            './src/**/*.{js,ts,tsx,jsx}', './src/*.{js,ts,tsx,jsx}',
-  ],
+                "./src/**/*.{js,ts,tsx,jsx}",
+                "./src/components/**/*.{js,ts,jsx,tsx}",
+                "./src/*.{js,jsx,ts,tsx}",
+           ],
   theme: {
     extend: {},
   },
