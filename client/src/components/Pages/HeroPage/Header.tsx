cea712d117c4b94effa1aa80d0d73d0796adeb08
kelvin magwe <kelvinmullar5@gmail.com>
2025-02-28 17:10:00 +0300

    Added header at the Hero(Website) with the different links and styling

--- client/src/components/Pages/HeroPage/Header.tsx
+++ client/src/components/Pages/HeroPage/Header.tsx
@@ -3,15 +3,29 @@ import React, { useState } from 'react';
 
 const Header: React.FC = () => {
     const [navigationItems, setNavigationItems] = useState(false);
-    
-    return (
-          <header className="bg-white px-6 py-4 justify-between items-center">
-                {/*logo*/}
-                <div className="text-2xl font-bold">
-                        <span>Kikao</span>
-                        <span>connect</span>
-                </div>
 
+    return (
+          <header className="bg-gray-800 shadow-md px-6 py-4 flex justify-between items-center">
+                  <div className="flex items-center space-x-6">
+                          {/*logo*/}
+                          <div className="text-2xl font-bold">
+                                  <span className="text-orange-400">Kikao</span>
+                                  <span className="text-white">connect</span>
+                          </div>
+
+                            {/*navigation items*/}
+                              <nav className="hidden text-white md:flex space-x-6">
+                                      <a href="/features">Features</a>
+                                      <a href="/pricing">Pricing</a>
+                                      <a href="/hiring">We are hiring</a>
+                                      <a href="/resources">Resources</a>
+                              </nav>
+                  </div>
+
+                <div className="text-white flex space-x-4 items-center">
+                        <a href="/login">Login</a>
+                        <button className="bg-orange-500 hover:bg-orange-600 text-white font-bold py-3 px-6 rounded-lg transition-all duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2" href="/demo">Request Demo</button>
+                </div>
           </header>
     )
 }
