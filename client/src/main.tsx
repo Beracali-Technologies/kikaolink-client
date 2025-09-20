e6e0c22d1a772dbcaa23ad3b3b3bc2f427a46f71
kelvin magwe <kelvinmullar5@gmail.com>
2025-09-20 11:27:42 +0300

    success deployment

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { AppInitializer } from './components/AppInitializer';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
           <AppInitializer>
                   <App />
+                    <SpeedInsights />
           </AppInitializer>
   </StrictMode>,
 )
