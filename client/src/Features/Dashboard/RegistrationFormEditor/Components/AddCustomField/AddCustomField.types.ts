62dcfcd05979dc91dfd8ffc28047dee0933a4484
kelvin magwe <141321238+kelvin-manyengo-magwe@users.noreply.github.com>
2025-09-24 23:13:52 +0800

    Delete client/src/Features/Dashboard/RegistrationFormEditor/Components directory

--- client/src/Features/Dashboard/RegistrationFormEditor/Components/AddCustomField/AddCustomField.types.ts
+++
@@ -1,5 +0,0 @@
-import { FieldType } from '@/types';
-
-export interface AddCustomFieldProps {
-  onAddField: (fieldType: FieldType) => void;
-}
--- client/src/Features/Dashboard/RegistrationFormEditor/Components/StandardFieldsPanel/StandardFieldsPanel.types.ts
+++
@@ -1,7 +0,0 @@
-import { Field } from '@/types';
-
-export interface StandardFieldsPanelProps {
-  allStandardFields: Field[];
-  activeFields: Field[];
-  onToggle: (systemName: string, isChecked: boolean) => void;
-}
